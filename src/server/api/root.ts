1acfb752dd99f433b9555a957eaac9ea4529dbac
SlaterRGordon <slats1999@gmail.com>
2023-04-24 12:32:27 -0700

    Add test router and simple queries

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
+import { testRouter } from "~/server/api/routers/test";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "~/server/api/routers/example";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  test: testRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/test.ts
@@ -0,0 +1,48 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+export const testRouter = createTRPCRouter({
+  getAll: publicProcedure
+    .input(z.object({ typeId: z.string(), orderBy: z.string(), order: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.test.findMany({
+        where: {
+          typeId: input.typeId,
+        },
+        orderBy: {
+          [input.orderBy]: input.order,
+        }
+      });
+    }),
+  getByUser: protectedProcedure
+    .input(z.object({ typeId: z.string(), orderBy: z.string(), order: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.test.findMany({
+        where: {
+          userId: ctx.session?.user.id,
+          typeId: input.typeId,
+        },
+        orderBy: {
+          [input.orderBy]: input.order,
+        }
+      });
+    }),
+  create: protectedProcedure
+    .input(z.object({ typeId: z.string(), speed: z.number(), accuracy: z.number() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.test.create({
+        data: {
+          userId: ctx.session?.user.id,
+          typeId: input.typeId,
+          speed: input.speed,
+          accuracy: input.accuracy,
+        },
+      });
+    })
+    
+});
