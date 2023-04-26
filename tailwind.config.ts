9e0d9a6c70989104b808279fd73c09a5e30a22a5
SlaterRGordon <slats1999@gmail.com>
2023-04-26 00:12:12 -0700

    daisyui themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,4 +9,7 @@ export default {
     require("daisyui"),
     require("@tailwindcss/typography"),
   ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  },
 } satisfies Config;
