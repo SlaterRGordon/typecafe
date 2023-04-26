a945877379c9e8a1dd46090151d6a93e1f01f81e
SlaterRGordon <slats1999@gmail.com>
2023-04-26 00:12:27 -0700

    add theme switcher to navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,41 +1,53 @@
 
 import { signIn, useSession } from "next-auth/react";
 import { Avatar } from "./Avatar";
+import { ThemeSwitch } from "./ThemeSwitch";
 
 export const Navigation = () => {
     const { data: sessionData } = useSession();
 
     return (
         <>
-            {/* Top Navigation, if user is not signed in */}
-            {!sessionData?.user ?
-                <div className="navbar">
-                    <div className="flex-1">
-                        <a className="btn btn-ghost normal-case text-xl">TypeCafe</a>
-                    </div>
-                    <div className="flex-none gap-2">
-                        {/* If the user is not signed in, display auth buttons */}
+            {/* Top Navigation */}
+            <div className="navbar bg-base-300">
+                <div className="flex-1">
+                    <a className="btn btn-ghost normal-case text-xl">TypeCafe</a>
+                </div>
+                <div className="flex-none gap-2 px-4">
+                    {/* Theme Switch */}
+                    <ThemeSwitch />
+
+                    {/* If the user is not signed in, display auth buttons */}
+                    {!sessionData?.user ?
                         <button
                             className="btn btn-primary btn-sm"
                             onClick={() => void signIn()}
                         >
                             Log In
                         </button>
-                    </div>
+                        :
+                        <>
+                            <button className="btn btn-ghost hover:btn-ghost">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M15.325 16.275q-.275-.325-.275-.737t.275-.688l1.85-1.85H10q-.425 0-.713-.288T9 12q0-.425.288-.713T10 11h7.175l-1.85-1.85q-.3-.3-.3-.713t.3-.712q.275-.3.688-.3t.687.275l3.6 3.6q.15.15.213.325t.062.375q0 .2-.062.375t-.213.325l-3.6 3.6q-.325.325-.713.288t-.662-.313ZM5 21q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h6q.425 0 .713.288T12 4q0 .425-.288.713T11 5H5v14h6q.425 0 .713.288T12 20q0 .425-.288.713T11 21H5Z" /></svg>
+                            </button>
+                            <Avatar />
+                        </>
+                    }
                 </div>
-                :
-                <Avatar />
-            }
+            </div>
+
 
             {/* Desktop Navigation */}
-            <div className="hidden md:flex">
-
+            <div className="fixed flex-col w-16 h-screen bg-base-200 hidden md:flex transition-all duration-100 ease-out hover:w-64">
+                <button className="btn btn-ghost">
+                    Slater
+                </button>
             </div>
 
             {/* Mobile Navigation */}
             <div className="flex md:hidden">
                 {/* Bottom Navigation */}
-                <div className="btm-nav">
+                <div className="btm-nav bg-base-200">
                     {/* Home */}
                     <button className="hover:active">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 19h3v-6h6v6h3v-9l-6-4.5L6 10v9Zm0 2q-.825 0-1.413-.588T4 19v-9q0-.475.213-.9t.587-.7l6-4.5q.275-.2.575-.3T12 3.5q.325 0 .625.1t.575.3l6 4.5q.375.275.588.7T20 10v9q0 .825-.588 1.413T18 21h-5v-6h-2v6H6Zm6-8.75Z" /></svg>
@@ -50,7 +62,7 @@ export const Navigation = () => {
                     </button>
                     {/* Color */}
                     <button className="hover:active">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 22C6.49 22 2 17.51 2 12S6.49 2 12 2s10 4.04 10 9c0 3.31-2.69 6-6 6h-1.77c-.28 0-.5.22-.5.5c0 .12.05.23.13.33c.41.47.64 1.06.64 1.67A2.5 2.5 0 0 1 12 22zm0-18c-4.41 0-8 3.59-8 8s3.59 8 8 8c.28 0 .5-.22.5-.5a.54.54 0 0 0-.14-.35c-.41-.46-.63-1.05-.63-1.65a2.5 2.5 0 0 1 2.5-2.5H16c2.21 0 4-1.79 4-4c0-3.86-3.59-7-8-7z"/><circle cx="6.5" cy="11.5" r="1.5" fill="currentColor"/><circle cx="9.5" cy="7.5" r="1.5" fill="currentColor"/><circle cx="14.5" cy="7.5" r="1.5" fill="currentColor"/><circle cx="17.5" cy="11.5" r="1.5" fill="currentColor"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 22C6.49 22 2 17.51 2 12S6.49 2 12 2s10 4.04 10 9c0 3.31-2.69 6-6 6h-1.77c-.28 0-.5.22-.5.5c0 .12.05.23.13.33c.41.47.64 1.06.64 1.67A2.5 2.5 0 0 1 12 22zm0-18c-4.41 0-8 3.59-8 8s3.59 8 8 8c.28 0 .5-.22.5-.5a.54.54 0 0 0-.14-.35c-.41-.46-.63-1.05-.63-1.65a2.5 2.5 0 0 1 2.5-2.5H16c2.21 0 4-1.79 4-4c0-3.86-3.59-7-8-7z" /><circle cx="6.5" cy="11.5" r="1.5" fill="currentColor" /><circle cx="9.5" cy="7.5" r="1.5" fill="currentColor" /><circle cx="14.5" cy="7.5" r="1.5" fill="currentColor" /><circle cx="17.5" cy="11.5" r="1.5" fill="currentColor" /></svg>
                     </button>
                 </div>
             </div>
