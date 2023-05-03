2eeb67f31de16ea1701d3428c6b83860175772d8
SlaterRGordon <slats1999@gmail.com>
2023-05-02 20:44:04 -0700

    ignore tailwind config and add variable colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,40 @@
 import { type Config } from "tailwindcss";
 
+// this function handles the opacity of color
+function withOpacityValue(variable: string) {
+  return ({ opacityValue }: any) => {
+    if (opacityValue === undefined) {
+      return `hsl(var(${variable}))`
+    }
+    return `hsl(var(${variable}) / ${opacityValue})`
+  }
+}
+
 export default {
   mode: 'jit',
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'retro': withOpacityValue('--retro'),
+        'valentine': withOpacityValue('--valentine'),
+        'cyberpunk': withOpacityValue('--cyberpunk'),
+      },
+    },
   },
   plugins: [
     require("daisyui"),
     require("@tailwindcss/typography"),
   ],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+    themes: ["light", "dark", "cyberpunk", "valentine", "acid",
+      {dark: {
+        ...require('daisyui/src/colors/themes')['[data-theme=dark]'],
+        "--retro": "45 47% 64%",
+        "--valentine": "318.46 46.429% 80.118%",
+        "--cyberpunk": "56 100% 45%",
+        "--rounded-btn": "1.9rem",
+      }}
+    ],
   },
 } satisfies Config;
