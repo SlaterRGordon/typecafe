1dd38d6f0e2a0a37505b8e4babaf1a647a0c0dfc
SlaterRGordon <slats1999@gmail.com>
2023-06-08 16:50:04 -0700

    fix text builder

--- src/components/typer/utils.tsx
+++ src/components/typer/utils.tsx
@@ -1,4 +1,4 @@
-import english10k from './english10k.json'
+import english10k from './languages/english10k.json'
 
 export const generateText = (count: number) => {
     let text = ''
@@ -14,16 +14,24 @@ export const generateText = (count: number) => {
     return text.slice(0, -1)
 }
 
-export const buildText = (text: string) => {
-    const textElements = []
+export const buildText = (text: string, index=0) => {
+    const words: JSX.Element[] = []
+    text.split(" ").forEach(word => {
+        const letters: JSX.Element[] = []
+        word.split("").forEach(letter => {
+            letters.push(<div key={index} id={index.toString()}>{letter}</div>);
+            index += 1;
+        })
 
-    for (let i = 0; i < text.length; i++) {
-        const char = text.charAt(i)
-        if (char === ' ') 
-            textElements.push(<div key={i} id={i.toString()}>&nbsp;</div>)
-        else 
-            textElements.push(<div key={i} id={i.toString()}>{char}</div>)
-    }
-    
-    return textElements
+        // add space to end of word
+        if (index != text.length) {
+            letters.push(<div key={index} id={index.toString()}>&nbsp;</div>);
+            index += 1;
+        }
+
+        // add word
+        words.push(<div key={index} className="inline-flex">{letters}</div>);
+    })
+
+    return words;
 }
