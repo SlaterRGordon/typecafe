1ab11f4f7258a66ea32fac9cd02bf510a12a617f
SlaterRGordon <slats1999@gmail.com>
2023-06-09 13:53:29 -0700

    force input to always focus

--- src/components/typer/Text.tsx
+++ src/components/typer/Text.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, createRef, useState } from "react"
 import { buildText } from "./utils"
 
 interface TextProps {
@@ -11,6 +11,9 @@ export const Text = (props: TextProps) => {
     // ref div to scroll text
     const typerRef = useRef(null)
 
+    // ref input to focus
+    const inputRef = useRef(null)
+
     const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
         console.log(e.key)
     }
@@ -19,9 +22,21 @@ export const Text = (props: TextProps) => {
         setElements(buildText(props.text))
     }, [props.text])
 
+    // event listeners to focus input
+    useEffect(() => {
+        window.addEventListener("click", () => {
+            const input = inputRef.current as HTMLInputElement | null
+            if (input) input.focus()
+        })
+        window.addEventListener("keydown", () => {
+            const input = inputRef.current as HTMLInputElement | null
+            if (input) input.focus()
+        })
+    }, [inputRef])
+
     return (
         <div className="flex flex-col max-h-24 w-8/12 overflow-hidden text-[22px] max-w-screen-xl z-30">
-            <input id="input" autoComplete="off" className="h-0 p-0 m-0 border-none" onKeyDown={handleKeyPress} autoFocus />
+            <input id="input" autoComplete="off" className="h-0 p-0 m-0 border-none" onKeyDown={handleKeyPress} ref={inputRef} autoFocus />
             <div className="flex flex-wrap justify-center overflow-y-hidden no-scrollbar scroll-smooth text-textPrimary select-none" id="words" ref={typerRef}>
                 {elements}
             </div>
