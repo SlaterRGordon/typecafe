cc5735b957fb49851718325c1730778d2fb28c54
SlaterRGordon <slats1999@gmail.com>
2023-06-18 14:58:40 -0700

    force max width when on mobile

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,7 +8,7 @@ export const Modal = (props: ModalProps) => {
         <>
             <input type="checkbox" id="configModal" className="modal-toggle" />
             <label htmlFor="configModal" className="modal modal-bottom !my-0 sm:modal-middle cursor-pointer">
-                <label htmlFor="" className="modal-box !w-[500px] h-[500px] !max-w-5xl space-y-2 !overflow-y-visible overflow-x-hidden">
+                <label htmlFor="" className="modal-box !sm:w-[500px] h-[500px] !max-w-5xl space-y-2 !overflow-y-visible overflow-x-hidden">
                     {props.children}
                 </label>
             </label>
