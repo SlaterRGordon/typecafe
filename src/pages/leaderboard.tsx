1a03a52fb530a0d0fc9b3503ef5921dc60b16769
SlaterRGordon <slats1999@gmail.com>
2023-07-04 11:45:53 -0700

    leaderboard filters

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -2,9 +2,13 @@ import { type NextPage } from "next";
 import { useState, useEffect } from "react";
 import { TestModes, TestSubModes } from "~/components/typer/types";
 import { api } from "~/utils/api";
+import Select, { ActionMeta, SingleValue } from 'react-select'
+
+type Option = { label: string, value: number }
 
 const Leadboard: NextPage = () => {
-    const [mode, setMode] = useState<TestModes>(TestModes.normal)
+    const mode = TestModes.normal
+    const [timeRange, setTimeRange] = useState(0)
     const [subMode, setSubMode] = useState<TestSubModes>(TestSubModes.timed)
     const [count, setCount] = useState(15)
 
@@ -17,11 +21,90 @@ const Leadboard: NextPage = () => {
         typeId: testType ? testType.id : ""
     })
 
+    const timeRangeOptions = [
+        { value: 0, label: 'Daily' },
+        { value: 1, label: 'Weekly' },
+        { value: 2, label: 'Monthly' },
+        { value: 2, label: 'All Time' },
+    ]
+    const handleChangeTimeRange = (value: SingleValue<Option>, actionMeta: ActionMeta<Option>) => {
+        if (value) setTimeRange(value.value)
+    }
+
+    const subModeOptions = [
+        { value: TestSubModes.timed, label: 'Timed' },
+        { value: TestSubModes.words, label: 'Words' },
+    ]
+
+    const timedCountOptions = [
+        { value: 15, label: '15' },
+        { value: 30, label: '30' },
+        { value: 60, label: '60' },
+        { value: 120, label: '120' },
+
+    ]
+    const handleChangeSubMode = (value: SingleValue<Option>, actionMeta: ActionMeta<Option>) => {
+        if (value) {
+            if (value.value == TestSubModes.timed) setCount(15)
+            else setCount(10)
+
+            setSubMode(value.value)
+        }
+    }
+
+    const wordsCountOptions = [
+        { value: 10, label: '10' },
+        { value: 25, label: '25' },
+        { value: 50, label: '50' },
+        { value: 100, label: '100' },
+    ]
+    const handleChangeCount = (value: SingleValue<Option>, actionMeta: ActionMeta<Option>) => {
+        if (value) setCount(value.value)
+    }
+
+    useEffect(() => {
+        void refetchTestType();
+        void refetchTests();
+    }, [subMode, count, refetchTestType, refetchTests])
+
     return (
         <>
             <div id="leaderboard" className="flex w-full h-full justify-center">
-                <div className="overflow-x-hidden overflow-y-hidden w-8/12 py-8">
-                    <table className="table table-zebra w-full">
+                <div className="flex flex-col overflow-x-auto overflow-y-hidden w-full mx-4 py-8 gap-2">
+                    <div className="flex gap-2">
+                        <Select
+                            instanceId="subModeSelect"
+                            defaultValue={subModeOptions[0]}
+                            options={subModeOptions}
+                            value={subModeOptions[subMode]}
+                            onChange={handleChangeSubMode}
+                            className="my-react-select-container"
+                            classNamePrefix="my-react-select"
+                        />
+                        <Select
+                            instanceId="countSelect"
+                            defaultValue={subMode == TestSubModes.timed ? timedCountOptions[0] : wordsCountOptions[0]}
+                            options={subMode == TestSubModes.timed ? timedCountOptions : wordsCountOptions}
+                            value={
+                                subMode == TestSubModes.timed ?
+                                    timedCountOptions.find(option => option.value == count) :
+                                    wordsCountOptions.find(option => option.value == count)
+                            }
+                            onChange={handleChangeCount}
+                            className="my-react-select-container"
+                            classNamePrefix="my-react-select"
+                        />
+                        <Select
+                            instanceId="timeRangeSelect"
+                            defaultValue={timeRangeOptions[0]}
+                            options={timeRangeOptions}
+                            value={timeRangeOptions[timeRange]}
+                            onChange={handleChangeTimeRange}
+                            className="my-react-select-container"
+                            classNamePrefix="my-react-select"
+                        />
+                    </div>
+                    <table className="table table-zebra w-full z-0">
                         <thead>
                             <tr>
                                 <th></th>
