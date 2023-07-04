44e080b877df36d4c1bb04380772b89055e6c1c3
SlaterRGordon <slats1999@gmail.com>
2023-07-03 20:48:15 -0700

    add routes to navigation

--- src/components/navigation/SideNavigation.tsx
+++ src/components/navigation/SideNavigation.tsx
@@ -1,8 +1,10 @@
 import { useSession } from "next-auth/react";
 import { useState } from "react";
 import { Avatar } from "../Avatar";
+import { useRouter } from "next/router";
 
 export const SideNavigation = () => {
+    const router = useRouter();
     const { data: sessionData } = useSession();
     const [isExpanded, setIsExpanded] = useState(false);
 
@@ -13,7 +15,7 @@ export const SideNavigation = () => {
             onMouseLeave={() => setIsExpanded(false)}
         >
             {/* Home */}
-            <button className={`btn btn-ghost m-2 flex flex-col content-start`}>
+            <button onClick={() => router.push('/')} className={`btn btn-ghost m-2 flex flex-col content-start`}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 19h3v-6h6v6h3v-9l-6-4.5L6 10v9Zm0 2q-.825 0-1.413-.588T4 19v-9q0-.475.213-.9t.587-.7l6-4.5q.275-.2.575-.3T12 3.5q.325 0 .625.1t.575.3l6 4.5q.375.275.588.7T20 10v9q0 .825-.588 1.413T18 21h-5v-6h-2v6H6Zm6-8.75Z" /></svg>
                 <div className={`text-lg normal-case ml-2 ${isExpanded ? 'block' : 'hidden'}`}>Home</div>
             </button>
@@ -23,7 +25,7 @@ export const SideNavigation = () => {
                 <div className={`text-lg normal-case ml-2 ${isExpanded ? 'block' : 'hidden'}`}>Practise</div>
             </button>
             {/* Leaderboard */}
-            <button className={`btn btn-ghost m-2 flex flex-col content-start`}>
+            <button onClick={() => router.push('/leaderboard')} className={`btn btn-ghost m-2 flex flex-col content-start`}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M4 11v8h4v-8H4Zm6-6v14h4V5h-4Zm6 8v6h4v-6h-4Zm4 8H4q-.825 0-1.413-.588T2 19v-8q0-.825.588-1.413T4 9h4V5q0-.825.588-1.413T10 3h4q.825 0 1.413.588T16 5v6h4q.825 0 1.413.588T22 13v6q0 .825-.588 1.413T20 21Z" /></svg>
                 <div className={`text-lg normal-case ml-2 ${isExpanded ? 'block' : 'hidden'}`}>Leaderboard</div>
             </button>
