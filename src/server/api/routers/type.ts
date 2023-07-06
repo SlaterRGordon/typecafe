875a4e1c7855818b6137697ceecb11fdc2e45dc6
SlaterRGordon <slats1999@gmail.com>
2023-07-06 13:36:34 -0700

    update api for languages

--- src/server/api/routers/type.ts
+++ src/server/api/routers/type.ts
@@ -7,13 +7,35 @@ import {
 
 export const typeRouter = createTRPCRouter({
   get: publicProcedure
-    .input(z.object({ mode: z.number(), subMode: z.number() }))
+    .input(z.object({ 
+      mode: z.number(), 
+      subMode: z.number(), 
+      language: z.string() 
+    }))
     .query(({ ctx, input }) => {
       return ctx.prisma.testType.findFirst({
         where: {
           mode: input.mode,
           subMode: input.subMode,
+          language: input.language,
         },
       });
     }),
+  create: publicProcedure
+    .input(z.object({
+      mode: z.number(),
+      subMode: z.number(),
+      language: z.string(),
+      competitive: z.boolean(),
+    }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.testType.create({
+        data: {
+          mode: input.mode,
+          subMode: input.subMode,
+          language: input.language,
+          competitive: input.competitive,
+        },
+      });
+    })
 });
