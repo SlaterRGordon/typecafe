a6386ee55bd998fbbd46bfa24f2649491fdf3670
SlaterRGordon <slats1999@gmail.com>
2023-07-11 20:45:03 -0700

    seperate leaderboard into own component

---
+++ src/components/scores/Scores.tsx
@@ -0,0 +1,124 @@
+import { useState, useEffect, useRef } from "react"
+import { TestModes, TestSubModes } from "~/components/typer/types"
+import { api } from "~/utils/api"
+import type { Test, User } from "@prisma/client"
+
+function isBottom(ref: React.RefObject<HTMLDivElement>) {
+    if (!ref.current) {
+        return false;
+    }
+    return ref.current.scrollHeight - ref.current.offsetHeight <= ref.current.scrollTop;
+}
+
+interface LeaderboardProps {
+    mode: TestModes,
+    subMode: TestSubModes,
+    count: number,
+    date: Date | undefined,
+}
+
+const Scores = (props: LeaderboardProps) => {
+    const { mode, subMode, count, date } = props;
+    const [allTests, setAllTests] = useState<(Test & {user: User;})[] | undefined>(undefined)
+    const limit = 16
+    const [page, setPage] = useState(0)
+
+    // scrollable div
+    const contentRef = useRef<HTMLTableSectionElement>(null);
+
+    // fetch types
+    const { data: testType } = api.type.get.useQuery({ mode, subMode, language: "english" })
+    const { data: tests, isLoading: isLoadingTests, isRefetching } = api.test.getAll.useQuery({
+        orderBy: "score",
+        order: "desc",
+        count,
+        date,
+        typeId: testType ? testType.id : "",
+        limit,
+        page,
+    })
+
+    // Hook on scroll
+    useEffect(() => {
+        const onScroll = () => {
+            if (isBottom(contentRef) && !isLoadingTests) {
+                setPage(page + 1)
+            }
+        };
+
+        const list = contentRef.current;
+        if (list) {
+            list.addEventListener('scroll', onScroll);
+            return () => {
+                list.removeEventListener('scroll', onScroll);
+            }
+        }
+    }, [contentRef, count, mode, date, limit, page, isLoadingTests]);
+
+    useEffect(() => {
+        if (tests && !isLoadingTests && !isRefetching) {
+            setAllTests(prevTests => {
+                if (prevTests) {
+                    const unqiueTests = [...prevTests, ...tests].filter(function(elem, pos) {
+                        return [prevTests, ...tests].indexOf(elem) == pos;
+                    }); 
+                    return unqiueTests
+                }
+                else return tests
+            })
+        }
+    }, [isLoadingTests, tests, isRefetching])
+
+    useEffect(() => {
+        setAllTests(undefined)
+        setPage(0)
+    }, [subMode, count, date])
+
+    return (
+        <>
+            <div id="leaderboard" className="flex w-full h-full justify-center">
+                <div className="flex flex-col overflow-x-auto overflow-y-hidden w-full gap-2">
+                    <div ref={contentRef} id="list" className="h-[100vh] overflow-auto">
+                        <table className="table table-zebra w-full z-0">
+                            <thead className="sticky top-0 z-50">
+                                <tr>
+                                    <th></th>
+                                    <th>User</th>
+                                    <th className="rounded-tr-lg sm:rounded-tr-none">WPM</th>
+                                    <th className="hidden rounded-tr-lg md:rounded-tr-none sm:table-cell">Accuracy</th>
+                                    <th className="hidden md:table-cell">Date</th>
+                                </tr>
+                            </thead>
+                            <tbody className="overflow-auto no-scrollbar">
+                                {allTests?.map((test, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <th>{index + 1}</th>
+                                            <td>
+                                                <div className="flex items-center space-x-3">
+                                                    <div className="avatar">
+                                                        <div className="mask mask-squircle w-12 h-12">
+                                                            <img src={test.user.image ?? ""} alt="" />
+                                                        </div>
+                                                    </div>
+                                                    <div>
+                                                        <div className="font-bold">{test.user.name}</div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td className={`${index + 1 == allTests.length ? "rounded-br-lg sm:rounded-br-none" : ""}`}>{test.speed.toFixed(2)}</td>
+                                            <td className={`${index + 1 == allTests.length ? "rounded-br-lg md:rounded-br-none" : ""} hidden sm:table-cell`}>{test.accuracy.toFixed(2)} %</td>
+                                            <td className="hidden md:table-cell">{test.createdAt.toLocaleDateString()}</td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Scores;
--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,80 +1,17 @@
 import { type NextPage } from "next"
-import { useState, useEffect, useRef } from "react"
+import { useState, useEffect } from "react"
 import { TestModes, TestSubModes } from "~/components/typer/types"
-import { api } from "~/utils/api"
 import type { SingleValue } from 'react-select'
 import Select from 'react-select'
-import type { Test, User } from "@prisma/client"
+import Scores from "~/components/scores/Scores"
 
 type Option = { label: string, value: number }
 
-function isBottom(ref: React.RefObject<HTMLDivElement>) {
-    if (!ref.current) {
-        return false;
-    }
-    return ref.current.scrollHeight - ref.current.offsetHeight <= ref.current.scrollTop;
-}
-
-const Leadboard: NextPage = () => {
-    const mode = TestModes.normal
-    const [allTests, setAllTests] = useState<(Test & {user: User;})[] | undefined>(undefined)
+const Leaderboard: NextPage = () => {
     const [date, setDate] = useState<Date | undefined>(undefined)
     const [timeRange, setTimeRange] = useState(0)
     const [subMode, setSubMode] = useState<TestSubModes>(TestSubModes.timed)
     const [count, setCount] = useState(15)
-    const limit = 16
-    const [page, setPage] = useState(0)
-
-    // scrollable div
-    const contentRef = useRef<HTMLTableSectionElement>(null);
-
-    // fetch types
-    const { data: testType } = api.type.get.useQuery({ mode, subMode, language: "english" })
-    const { data: tests, isLoading: isLoadingTests, isRefetching } = api.test.getAll.useQuery({
-        orderBy: "score",
-        order: "desc",
-        count,
-        date,
-        typeId: testType ? testType.id : "",
-        limit,
-        page,
-    })
-
-    // Hook on scroll
-    useEffect(() => {
-        const onScroll = () => {
-            if (isBottom(contentRef) && !isLoadingTests) {
-                setPage(page + 1)
-            }
-        };
-
-        const list = contentRef.current;
-        if (list) {
-            list.addEventListener('scroll', onScroll);
-            return () => {
-                list.removeEventListener('scroll', onScroll);
-            }
-        }
-    }, [contentRef, count, mode, date, limit, page, isLoadingTests]);
-
-    useEffect(() => {
-        if (tests && !isLoadingTests && !isRefetching) {
-            setAllTests(prevTests => {
-                if (prevTests) {
-                    const unqiueTests = [...prevTests, ...tests].filter(function(elem, pos) {
-                        return [prevTests, ...tests].indexOf(elem) == pos;
-                    }); 
-                    return unqiueTests
-                }
-                else return tests
-            })
-        }
-    }, [isLoadingTests, tests, isRefetching])
-
-    useEffect(() => {
-        setAllTests(undefined)
-        setPage(0)
-    }, [subMode, count, date])
 
     const timeRangeOptions = [
         { value: 0, label: 'Daily' },
@@ -180,47 +117,11 @@ const Leadboard: NextPage = () => {
                             classNamePrefix="my-react-select"
                         />
                     </div>
-                    <div ref={contentRef} id="list" className="h-[100vh] overflow-auto">
-                        <table className="table table-zebra w-full z-0">
-                            <thead className="sticky top-0 z-50">
-                                <tr>
-                                    <th></th>
-                                    <th>User</th>
-                                    <th className="rounded-tr-lg sm:rounded-tr-none">WPM</th>
-                                    <th className="hidden rounded-tr-lg md:rounded-tr-none sm:table-cell">Accuracy</th>
-                                    <th className="hidden md:table-cell">Date</th>
-                                </tr>
-                            </thead>
-                            <tbody className="overflow-auto no-scrollbar">
-                                {allTests?.map((test, index) => {
-                                    return (
-                                        <tr key={index}>
-                                            <th>{index + 1}</th>
-                                            <td>
-                                                <div className="flex items-center space-x-3">
-                                                    <div className="avatar">
-                                                        <div className="mask mask-squircle w-12 h-12">
-                                                            <img src={test.user.image ?? ""} alt="" />
-                                                        </div>
-                                                    </div>
-                                                    <div>
-                                                        <div className="font-bold">{test.user.name}</div>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td className={`${index + 1 == allTests.length ? "rounded-br-lg sm:rounded-br-none" : ""}`}>{test.speed.toFixed(2)}</td>
-                                            <td className={`${index + 1 == allTests.length ? "rounded-br-lg md:rounded-br-none" : ""} hidden sm:table-cell`}>{test.accuracy.toFixed(2)} %</td>
-                                            <td className="hidden md:table-cell">{test.createdAt.toLocaleDateString()}</td>
-                                        </tr>
-                                    )
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
+                    <Scores mode={TestModes.normal} subMode={subMode} count={count} date={date} />
                 </div>
             </div>
         </>
     )
 }
 
-export default Leadboard;
+export default Leaderboard;
