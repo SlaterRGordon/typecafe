4a4fd73431eb8b9d0332cc6e951158003340862d
SlaterRGordon <slats1999@gmail.com>
2023-07-19 09:21:57 -0700

    change container for avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -9,14 +9,19 @@ export const Avatar = (props: AvatarProps) => {
     const { data: sessionData } = useSession();
 
     return (
-        <div className="avatar placeholder">
-            <div style={{maxWidth: props.size, minWidth: '1.5rem'}} className="rounded-full bg-neutral-focus text-neutral-content">
-                {sessionData?.user?.image ?
-                    <Image width={500} height={500} src={sessionData.user.image} alt={sessionData.user.name ?? ""} referrerPolicy="no-referrer" />
-                    :
-                    <span>AA</span>
-                }
+        <div className="avatar">
+            <div style={{maxWidth: props.size, minWidth: '1.5rem'}} className="mask mask-circle">
+                <Image width={500} height={500} src={sessionData?.user.image ?? ""} alt="" />
             </div>
         </div>
+        // <div className="avatar placeholder">
+        //     <div style={{maxWidth: props.size, minWidth: '1.5rem'}} className="rounded-full bg-neutral-focus text-neutral-content">
+        //         {sessionData?.user?.image ?
+        //             <Image width={500} height={500} src={sessionData.user.image} alt={sessionData.user.name ?? ""} referrerPolicy="no-referrer" />
+        //             :
+        //             <span>AA</span>
+        //         }
+        //     </div>
+        // </div>
     )
 }
