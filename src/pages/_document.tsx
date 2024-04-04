d59e92d56abcce00c7ccec457506f40296f79505
SlaterRGordon <slats1999@gmail.com>
2024-04-04 14:54:49 -0700

    add analytics tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,23 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
     return (
         <Html data-theme="dark">
             <Head />
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-16KETVK938"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){window.dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-16KETVK938');
+                `}
+            </Script>
             <body className="overflow-hidden">
                 <Main />
                 <NextScript />
