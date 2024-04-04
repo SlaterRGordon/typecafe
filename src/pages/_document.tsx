11356b841da44f36790433eb32b7678875b9f9aa
SlaterRGordon <slats1999@gmail.com>
2024-04-04 15:40:42 -0700

    update analytics tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,20 +4,21 @@ import Script from "next/script";
 export default function Document() {
     return (
         <Html data-theme="dark">
-            <Head />
-            <Script
-                src="https://www.googletagmanager.com/gtag/js?id=G-16KETVK938"
-                strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-                {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){window.dataLayer.push(arguments);}
-                gtag('js', new Date());
+            <Head>
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-16KETVK938"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){window.dataLayer.push(arguments);}
+                    gtag('js', new Date());
 
-                gtag('config', 'G-16KETVK938');
-                `}
-            </Script>
+                    gtag('config', 'G-16KETVK938');
+                    `}
+                </Script>
+            </Head>
             <body className="overflow-hidden">
                 <Main />
                 <NextScript />
