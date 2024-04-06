a5060beea68b4541c46fead8939f3f33cab90fd6
SlaterRGordon <slats1999@gmail.com>
2024-04-06 11:25:24 -0700

    update icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,9 @@ export default function Document() {
                     gtag('config', 'G-16KETVK938');
                     `}
                 </Script>
+                <link rel="shortcut icon" href="/images/favicon.ico" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
             </Head>
             <body className="overflow-hidden">
                 <Main />
