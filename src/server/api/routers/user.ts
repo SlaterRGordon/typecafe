f7a67cc08ad1d1a828747f2302ceac29c00de61e
SlaterRGordon <slats1999@gmail.com>
2024-04-05 20:18:18 -0700

    add user delete endpoint

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -32,4 +32,12 @@ export const userRouter = createTRPCRouter({
                 },
             });
         }),
+    delete: protectedProcedure
+        .mutation(({ ctx }) => {
+            return ctx.prisma.user.delete({
+                where: {
+                    id: ctx.session?.user.id,
+                },
+            });
+        }),
 });
