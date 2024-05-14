bed7b3e277b0fe051a3a888f14a211a76fdc8607
SlaterRGordon <slats1999@gmail.com>
2024-05-13 19:39:20 -0700

    fix empty img avatars

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -12,10 +12,10 @@ export const Avatar = (props: AvatarProps) => {
         <div className="avatar placeholder">
             <div style={{ width: props.size, minWidth: '1.5rem' }} className="mask mask-circle">
                 {sessionData?.user.image ?
-                    <Image width={500} height={500} src={sessionData?.user.image ?? ""} alt="Profile Picture" referrerPolicy="no-referrer" />
+                    <Image className="rounded-full" width={500} height={500} src={sessionData?.user.image ?? ""} alt="Profile Picture" referrerPolicy="no-referrer" />
                     :
                     <div className="avatar placeholder">
-                        <div className="bg-neutral text-neutral-content rounded-full w-24">
+                        <div className="bg-neutral text-white rounded-full w-24">
                             <span className="text-md">{sessionData?.user.username?.charAt(0).toUpperCase() ?? ""}</span>
                         </div>
                     </div>
